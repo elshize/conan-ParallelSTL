aaf5bd884edb9b0d5d7bf1571676f42670c58bfc
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-11-01 19:38:15 -0400

    find_package compatibility

--- conanfile.py
+++ conanfile.py
@@ -7,14 +7,24 @@ class ParallelSTLConan(ConanFile):
     license = "Apache License 2.0"
     url = "https://github.com/elshize/conan-ParallelSTL"
     code_url = "https://github.com/intel/parallelstl/"
-    description = "JSON for Modern C++"
+    generators = "cmake_paths"
 
     def source(self):
+        self.run("git clone https://github.com/justusc/FindTBB.git")
         self.run("git clone %s" % self.code_url)
+        #self.run("cp FindTBB/FindTBB.cmake parallelstl")
         self.run("cd parallelstl && git checkout %s" % self.version)
 
     def package(self):
+        tools.replace_in_file("parallelstl/CMakeLists.txt",
+                "project(ParallelSTL VERSION ${VERSION_MAJOR}.${VERSION_MINOR} LANGUAGES CXX)",
+                '''project(ParallelSTL VERSION ${VERSION_MAJOR}.${VERSION_MINOR} LANGUAGES CXX)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../FindTBB")''')
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = 'conan_paths.cmake'
+        cmake.configure(source_dir='parallelstl')
         self.copy("*", dst="include", src="parallelstl/include")
+        self.copy("*.cmake")
 
     def requirements(self):
         self.requires("TBB/2018_U5@conan/stable")
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -5,10 +5,11 @@ from conans import ConanFile, CMake, tools
 
 class ParallelSTLTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake_paths"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = 'conan_paths.cmake'
         cmake.configure()
         cmake.build()
 
@@ -18,6 +19,4 @@ class ParallelSTLTestConan(ConanFile):
         self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+        self.run(".%sexample" % os.sep)
